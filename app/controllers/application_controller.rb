249cf5939d3b41bb2526eba0d6b6075cc6cc54dd
rrmorant <duecel@icloud.com>
2023-12-06 15:30:41 -0800

    backend structurecompleted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+    skip_before-action :verify_authenticity_token
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+dogs = [
+    {
+        name: 'Felix',
+        age: 2,
+        enjoys: 'Long naps on the couch, and a warm fire',
+        image: 'https://images.unsplash.com/photo-1561037404-61cd46aa615b?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Homer',
+        age: 12,
+        enjoys: 'Food mostly, really just food.',
+        image: 'https://images.unsplash.com/photo-1587518102280-8d5fdcb68d13?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+        name: 'Jack',
+        age: 5,
+        enjoys: 'Furrrrociously hunting bugs.',
+        image: 'https://images.unsplash.com/photo-1517849845537-4d257902454a?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    }
+]
+
+dogs.each do |each_dog|
+    Dog.create each_dog
+    puts "creating dog #{each_dog}"
+end
