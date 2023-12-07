75589658ef9ab72a321a5c6ed4f5fde87a2f7d2e
Jackson Pruett <jacksonpruett@Jacksons-MBP.lan>
2023-12-07 15:04:29 -0500

    api endpoints completed

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -5,12 +5,25 @@ class DogsController < ApplicationController
     end
 
     def create
+        dog = Dog.create(dog_params)
+        render json: dog
     end
 
     def update
+        dog = Dog.find(params[:id])
+        dog.update(dog_params)
+        render json: dog
     end
 
     def destroy
+        dog = Dog.find(params[:id])
+        dog.destroy
+        render json: dog
     end
 
+    private
+    # strong params
+    def dog_params
+        params.require(:dog).permit(:name, :age, :enjoys, :image)
+    end
 end
